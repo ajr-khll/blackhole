ea8bedc3c7b0b072bd324f48409dd1cf92d52820
ajr-khll <machacks02@gmail.com>
2026-01-02 18:16:03 +0800

    Linked GLFW and glad

---
+++ blackhole/src/main.cpp
@@ -0,0 +1,48 @@
+
+
+#include <glad/glad.h>
+//
+#include <GLFW/glfw3.h>
+//
+#include <iostream>
+
+int main() {
+  if (!glfwInit()) {
+    std::cerr << "Failed to init GLFW\n";
+    return -1;
+  }
+
+  // macOS requirements
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+  GLFWwindow *window =
+      glfwCreateWindow(800, 600, "OpenGL on Apple Silicon", nullptr, nullptr);
+
+  if (!window) {
+    std::cerr << "Failed to create window\n";
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cerr << "Failed to initialize GLAD\n";
+    return -1;
+  }
+
+  std::cout << "OpenGL version: " << glGetString(GL_VERSION) << "\n";
+
+  while (!glfwWindowShouldClose(window)) {
+    glClearColor(0.1f, 0.1f, 0.15f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+}
